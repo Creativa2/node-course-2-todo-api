abd8eb61d8b5c999351c11c71d96b197c1211b00
Raiiny <benedek200@gmail.com>
2017-09-05 14:10:54 +0200

    Add tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -113,7 +113,6 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
-
 app.post('/users/login', (req, res) =>{
   var body = _.pick(req.body, ['email', 'password']);
 
